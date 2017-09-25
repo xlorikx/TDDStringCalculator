f55590025b1f211242b545f55042d114a127841e
Iurii_Deiev <Iurii_Deiev@epam.com>
2017-09-25 13:10:44 +0300

    implementation refactoring

--- src/main/java/com/deiev/stringcalculator/StringCalculator.java
+++ src/main/java/com/deiev/stringcalculator/StringCalculator.java
@@ -1,21 +1,40 @@
 package com.deiev.stringcalculator;
 
-import static org.apache.commons.lang3.StringUtils.chomp;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 import static org.apache.commons.lang3.StringUtils.isNotBlank;
 
 class StringCalculator {
 
-    int add(String numbers) {
-        int result = 0;
+    int add(String stringNumbers) {
+        List<Integer> numbers = Collections.emptyList();
 
-        if (isNotBlank(numbers)) {
-            if (numbers.contains(",")) {
-                for (String digit : numbers.split(",")) {
-                    result += Integer.parseInt(digit);
-                }
-            } else result = Integer.valueOf(numbers);
+        if (isNotBlank(stringNumbers)) {
+            numbers = parseNumbers(stringNumbers);
+        }
+
+        return sum(numbers);
+    }
+
+    private List<Integer> parseNumbers(String numbers) {
+        List<String> separated = Arrays.asList(numbers.split(","));
+        List<Integer> result = new ArrayList<>(separated.size());
+
+        for (String s : separated) {
+            result.add(Integer.parseInt(s));
         }
 
         return result;
+    }
+
+    private int sum(List<Integer> terms) {
+        int sum = 0;
+        for (Integer term : terms) {
+            sum += term;
+        }
+        return sum;
     }
 }
